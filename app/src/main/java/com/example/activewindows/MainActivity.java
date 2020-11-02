f3aa231302bb35137985c033a21c0cfe16ff7af6
JonathanClifford <joncliffjr@gmail.com>
2020-11-01 23:18:17 -0500

    Update as of 11/1/2020 - Established official communication to AWS - data shows as connecting - MQTT and other necessary information has been imported, including info on what AWS will be handling and certain things names. - Also set up the basic functionality on the item select java app -> After this, we will establish teh main connection in here. - In the item select subcomponent, we will need to implement each of the three buttons to send a specific command (1,0.5,0) to send to AWS. - Similarly, the monitor for the current status topic will also need to updated in this location as well to take in the most up to date status from AWS topic.

--- app/src/main/java/com/example/activewindows/MainActivity.java
+++ app/src/main/java/com/example/activewindows/MainActivity.java
@@ -33,7 +33,48 @@ import android.widget.Toast;
 import java.text.SimpleDateFormat; // Used to display the date.
 import java.util.Date;
 import java.util.Locale;
+// Import necessary info for AWS
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Regions;
+
+// The following tutorial is incredably helpful on understanding AWS -> Android App stuff
+// https://www.linkedin.com/pulse/android-app-aws-iot-core-guide-felipe-ramos-da-silva
+// The same can be said with this video for the future although this mainly deals with microcontrollers:
+// https://youtu.be/LNVRzr4oDW0
+// https://github.com/felipemeriga/aws-sdk-android-samples
+
+// _________________________________________________________________________________________
+// Various info for my own knowledge
+// Cognito Identities consist of the following:
+// ActiveWindowsIoT
+// IAM Roles:
+// Cognito_ActiveWindowsIoTAuth_Role => Authenticated Role
+// Cognito_ActiveWindowsIoTUnauth_Role => Unauthenticated Role
+// Both of these Roles were given IoTFullAccess Policies
+// Iot Core:
+// This is referenced as "ActiveWindowsAndroidApp" for a thing
+// HTTPS
+// Update your Thing Shadow using this Rest API Endpoint.
+// aga9o21zvh26x-ats.iot.us-east-1.amazonaws.com
+// This app will SUBSCRIBE to:
+// windowStatusTopic
+// This app will PUBLISH to:
+// windowCommandTopic
+// _________________________________________________________________________________________
 
 
 
@@ -42,13 +83,101 @@ import java.util.Locale;
 // Normally there is no 'Implements AdapterView' section, but this is here to help implement our
 // Spinner in the UI
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    static final String LOG_TAG = MainActivity.class.getCanonicalName();
+    // CREATE INFORMATION FOR AWS.
+    //______________________________________________________________________________________________
+    // AWS - for AWS Thing (ActiveWindowsAndroidApp thing)
+    public static final String CUSTOMER_SPECIFIC_ENDPOINT = "aga9o21zvh26x-ats.iot.us-east-1.amazonaws.com";
+    // For the COGNITO roles that we are using (Cognito_ActiveWindowsIoTAuth_Role / unauth)
+    public static final String COGNITO_POOL_ID = "us-east-1:a2ebd097-89fc-44a3-9cda-c0dcdac34457";
+    // AWS Region
+    public static final Regions MY_REGION = Regions.US_EAST_1; // TODO Note these are originally private
+    public AWSIotMqttManager mqttManager; // manager for MQTT
+    public String clientId; // Client ID, since this needs to be unique for the users, we will be using it.
+    public CognitoCachingCredentialsProvider credentialsProvider; //For credentials to connect to AWS
+    // FOR SOME REASON: The above code can't be referenced in the other activity to actively see data.
+    // So basically will need to remake it in the item select code.
+    TextView currentStatus; // This var is to keep track of connectivity to aws
 
+
+
+    // START APPLICATION
+    //_____________________________________________________________________________________________
     @Override
     protected void onCreate(Bundle savedInstanceState) { // This gets called all the time once the
         // App starts. This is effectively the Main method, but for Android Studio.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentStatus = (TextView) findViewById(R.id.currentStatusView); //grab
 
+        //SET UP MORE INFORMATION FOR AWS.
+        //_________________________________________________________________________________________
+        // MQTT client IDs are required to be unique per AWS IoT account.
+        // This UUID is "practically unique" but does not _guarantee_
+        // uniqueness.
+        //clientId = UUID.randomUUID().toString();
+
+        // MQTT client IDs are required to be unique per AWS IoT account.
+        // This UUID is "practically unique" but does not _guarantee_
+        // uniqueness.
+        clientId = UUID.randomUUID().toString();
+        // Initialize the AWS Cognito credentials provider
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(), // context
+                COGNITO_POOL_ID, // Identity Pool ID
+                MY_REGION // Region
+        );
+
+        // MQTT Client
+        mqttManager = new AWSIotMqttManager(clientId, CUSTOMER_SPECIFIC_ENDPOINT);
+
+        // Check MQTT connectrion response when attempting to connect to the servers.
+        try {
+            mqttManager.connect(credentialsProvider, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status, final Throwable throwable) {
+                    Log.d(LOG_TAG, "Status = " + String.valueOf(status));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (status == AWSIotMqttClientStatus.Connecting) {
+                                currentStatus.setText("Connecting...");
+
+                            } else if (status == AWSIotMqttClientStatus.Connected) {
+                                currentStatus.setText("Connected");
+
+                            } else if (status == AWSIotMqttClientStatus.Reconnecting) {
+                                if (throwable != null) {
+                                    Log.e(LOG_TAG, "Connection error.", throwable);
+                                }
+                                currentStatus.setText("Reconnecting");
+                            } else if (status == AWSIotMqttClientStatus.ConnectionLost) {
+                                if (throwable != null) {
+                                    Log.e(LOG_TAG, "Connection error.", throwable);
+                                    throwable.printStackTrace();
+                                }
+                                currentStatus.setText("Disconnected");
+                            } else {
+                                currentStatus.setText("Disconnected");
+
+                            }
+                        }
+                    });
+                }
+            });
+        } catch (final Exception e) { //If there is an error, display it.
+            Log.e(LOG_TAG, "Connection error.", e);
+            currentStatus.setText("Error! " + e.getMessage());
+        }
+
+
+
+
+
+
+        // ________________________________________________________________________________________
+        // Create UI Elements primarily for date and time
         //Create a string that gets recognized as the date. Change the display file to match
         String theDate_str = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date()); // This just sets a date
         TextView textDate = (TextView)findViewById(R.id.editTextDate); // This is for the text date
@@ -66,7 +195,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         welcomeUser.setText("Welcome, " + userName + " !");
 
         // Create the spinner. This is used as a drop down menu that the user will select each of
-        // The windows from.  TODO changed this to final
+        // The windows from.
         final Spinner windowSpinner = (Spinner) findViewById(R.id.spinner1);
 
         //ArrayAdapter is the container that holds the values and then integrate w/ spinner
--- app/src/main/java/com/example/activewindows/item_select.java
+++ app/src/main/java/com/example/activewindows/item_select.java
@@ -6,6 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat; // Used to display the date.
+import java.util.Date;
+import java.util.Locale;
+// Import necessary info for AWS
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Regions;
+
 
 // This file is dedicated to essentially loading another activity (Think of it as another class)
 // That will load the data for the specific window and send the data to AWS.
@@ -19,13 +51,106 @@ public class item_select extends AppCompatActivity {
     Button back; // I have a back button instead of re-implementing the spinner in this class.
     // It would be straightforward to implement but for testing this is the most straightforward.
 
+    // Select all of the buttons and instantiate them as existing.
+    Button openWindow;
+    Button crackWindow;
+    Button closeWindow;
+    TextView currentStatus;
+    TextView latestMessage;
+    // CREATE INFORMATION FOR AWS.
+    //______________________________________________________________________________________________
+    // AWS - for AWS Thing (ActiveWindowsAndroidApp thing)
+    public static final String CUSTOMER_SPECIFIC_ENDPOINT = "aga9o21zvh26x-ats.iot.us-east-1.amazonaws.com";
+    // For the COGNITO roles that we are using (Cognito_ActiveWindowsIoTAuth_Role / unauth)
+    public static final String COGNITO_POOL_ID = "us-east-1:a2ebd097-89fc-44a3-9cda-c0dcdac34457";
+    // AWS Region
+    public static final Regions MY_REGION = Regions.US_EAST_1; // TODO Note these are originally private
+    public AWSIotMqttManager mqttManager; // manager for MQTT
+    public String clientId; // Client ID, since this needs to be unique for the users, we will be using it.
+    public CognitoCachingCredentialsProvider credentialsProvider; //For credentials to connect to AWS
+    static final String LOG_TAG = item_select.class.getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_select);
 
-        back = findViewById(R.id.TestButton);
 
+        //Select buttons and recognize them for later on.
+        back = findViewById(R.id.TestButton);
+        openWindow = findViewById(R.id.OpenWindowButton); // make it the open window button
+        crackWindow = findViewById(R.id.CrackWindowButton);
+        closeWindow = findViewById(R.id.CloseWindowButton);
+        currentStatus = (TextView) findViewById(R.id.currentStatusView2);
+        latestMessage = (TextView) findViewById(R.id.latestMessage);
+
+        clientId = UUID.randomUUID().toString();
+        // Initialize the AWS Cognito credentials provider
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(), // context
+                COGNITO_POOL_ID, // Identity Pool ID
+                MY_REGION // Region
+        );
+
+        // MQTT Client
+        mqttManager = new AWSIotMqttManager(clientId, CUSTOMER_SPECIFIC_ENDPOINT);
+
+        // Check MQTT connectrion response when attempting to connect to the servers.
+        try {
+            mqttManager.connect(credentialsProvider, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status, final Throwable throwable) {
+                    Log.d(LOG_TAG, "Status = " + String.valueOf(status));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (status == AWSIotMqttClientStatus.Connecting) {
+                                currentStatus.setText("Connecting...");
+
+                            } else if (status == AWSIotMqttClientStatus.Connected) {
+                                currentStatus.setText("Connected");
+
+                            } else if (status == AWSIotMqttClientStatus.Reconnecting) {
+                                if (throwable != null) {
+                                    Log.e(LOG_TAG, "Connection error.", throwable);
+                                }
+                                currentStatus.setText("Reconnecting");
+                            } else if (status == AWSIotMqttClientStatus.ConnectionLost) {
+                                if (throwable != null) {
+                                    Log.e(LOG_TAG, "Connection error.", throwable);
+                                    throwable.printStackTrace();
+                                }
+                                currentStatus.setText("Disconnected");
+                            } else {
+                                currentStatus.setText("Disconnected");
+
+                            }
+                        }
+                    });
+                }
+            });
+        } catch (final Exception e) { //If there is an error, display it.
+            Log.e(LOG_TAG, "Connection error.", e);
+            currentStatus.setText("Error! " + e.getMessage());
+        }
+
+        //__________________________________________________________________________________________
+        // Publish and subscribe to topics.
+        final String topic_sub = "windowStatusTopic"; //THIS IS THE TOPIC WE WILL SUBSCRIBE TO
+        final String topic_pub = "windowCommandTopic"; //THIS IS THE TOPIC WE WILL PUBLISH TO
+        //__________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+        //_________________________________________________________________________________________
+        // Set button clickers.
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(item_select.this, MainActivity.class);
@@ -33,6 +158,41 @@ public class item_select extends AppCompatActivity {
             }
         });
 
+        openWindow.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //TODO: SEND THIS DATA TO THE TOPIC TO OPEN THE WINDOW!
+                String command = "Open Window";
+                Toast.makeText(item_select.this, command, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        crackWindow.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //TODO: SEND THIS DATA TO THE TOPIC TO OPEN THE WINDOW!
+                String command = "Crack Window";
+                Toast.makeText(item_select.this, command, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        closeWindow.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //TODO: SEND THIS DATA TO THE TOPIC TO OPEN THE WINDOW!
+                String command = "Close Window";
+                Toast.makeText(item_select.this, command, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
